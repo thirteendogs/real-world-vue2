9c225ed45e05a61407a98030e6f5f2d1129be038
thirteendogs <daniel.nova.girbau@gmail.com>
2022-07-27 23:41:05 +0200

    Add basic router

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,26 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import EventList from "../views/EventList.vue";
+import EventShow from "../views/EventShow.vue";
+import EventCreate from "../views/EventCreate.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "home",
-    component: HomeView,
+    name: "event-list",
+    component: EventList,
   },
   {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    path: "/event",
+    name: "event-show",
+    component: EventShow,
+  },
+  {
+    path: "/event/create",
+    name: "event-create",
+    component: EventCreate,
   },
 ];
 
